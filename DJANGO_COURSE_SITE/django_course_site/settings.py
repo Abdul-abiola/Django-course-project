6eb6961634513a4be9c53a774178e188f31899ad
Abdul-abiola <abdulrahamanabdulrahaman03@gmail.com>
2026-03-18 12:29:51 -0700

    Update settings.py

--- DJANGO_COURSE_SITE/django_course_site/settings.py
+++ DJANGO_COURSE_SITE/django_course_site/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR/'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'uploads'
 MEDIA_URL =  '/files/'
